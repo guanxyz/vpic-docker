dd233d597e8afdc249af6cc4a1d636f7aa4aecc7
Tomyao <Tomyao.l@gmail.com>
2016-12-14 16:01:47 -0700

    Update runvpic.sh

--- runvpic.sh
+++ runvpic.sh
@@ -15,7 +15,8 @@ cd /mnt/vpicrun/vpic.bin
 export CPLUS_INCLUDE_PATH=/mnt/vpicrun/vpic/src/util/catalyst/
 mkdir -p /mnt/vpicrun/run1
 cd /mnt/vpicrun/run1
-/mnt/vpicrun/vpic.bin/bin/vpic ../vpic/sample/8preconnection.cxx
+../vpic.bin/bin/vpic ../vpic/sample/8preconnection.cxx
+cp ../vpic/sample/contourbenchmark.py ./insitu.py
 export LD_LIBRARY_PATH=/usr/local/paraview.bin/lib
 echo "Sleeping 5 to wait for filehandle."
 sleep 5
