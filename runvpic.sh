4e8db9652f1ea0eb77269dfdaa89dbef55bafee2
Tomyao <Tomyao.l@gmail.com>
2016-12-15 14:56:05 -0700

    Update runvpic.sh

--- runvpic.sh
+++ runvpic.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
+# Update VPIC
+cd /mnt/vpicrun/vpic
+git pull
+
 # Build VPIC
 cd /mnt/vpicrun/vpic.bin
 cmake \
@@ -11,14 +15,29 @@ make -j16
 # PUT RUNS BELOW
 
 # Run 1
+#cd /mnt/vpicrun/vpic.bin
+#export CPLUS_INCLUDE_PATH=/mnt/vpicrun/vpic/src/util/catalyst/
+#mkdir -p /mnt/vpicrun/run1
+#cd /mnt/vpicrun/run1
+#../vpic.bin/bin/vpic ../vpic/sample/8preconnection.cxx
+#cp ../vpic/sample/contourbenchmark.py ./insitu.py
+#export LD_LIBRARY_PATH=/usr/local/paraview.bin/lib
+#echo "Sleeping 5 to wait for filehandle."
+#sleep 5
+#echo "Launching 8preconnection"
+#LD_LIBRARY_PATH=/usr/local/paraview.bin/lib mpiexec -machinefile /mnt/vpicrun/machinefile /mnt/vpicrun/run1/8preconnection.Linux
+
+# Run 2
 cd /mnt/vpicrun/vpic.bin
 export CPLUS_INCLUDE_PATH=/mnt/vpicrun/vpic/src/util/catalyst/
-mkdir -p /mnt/vpicrun/run1
-cd /mnt/vpicrun/run1
-../vpic.bin/bin/vpic ../vpic/sample/8preconnection.cxx
-cp ../vpic/sample/contourbenchmark.py ./insitu.py
+mkdir -p /mnt/vpicrun/run2
+cd /mnt/vpicrun/run2
+../vpic.bin/bin/vpic ../vpic/sample/turbulence_master.cxx
+cp ../vpic/sample/threshold.py ./insitu.py
+cp ../vpic/sample/benchmark.py .
+cp ../vpic/sample/parse_timings.py .
 export LD_LIBRARY_PATH=/usr/local/paraview.bin/lib
 echo "Sleeping 5 to wait for filehandle."
 sleep 5
 echo "Launching 8preconnection"
-LD_LIBRARY_PATH=/usr/local/paraview.bin/lib mpiexec -machinefile /mnt/vpicrun/machinefile /mnt/vpicrun/run1/8preconnection.Linux
+LD_LIBRARY_PATH=/usr/local/paraview.bin/lib mpiexec -machinefile /mnt/vpicrun/machinefile /mnt/vpicrun/run2/turbulence_master.Linux
